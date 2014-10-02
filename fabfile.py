9992bda81023b3017a3f3a7a6e3711cf2cf3f0c4
Jesse B Miller <jmiller@adops.com>
2014-10-02 15:58:28 -0500

    updates fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,4 @@
-import urllib
 from ConfigParser import ConfigParser
-
 from fabric.api import *
 
 manifest = ConfigParser()
@@ -8,8 +6,8 @@ manifest.read('Manifest')
 
 service_name = manifest.get('Service', 'name')
 unittest_cmd = manifest.get('Service', 'unittest_cmd')
-accept_cmd = manifest.get('Service', 'accept_cmd')
-sanity_cmd = manifest.get('Service', 'sanity_cmd')
+# accept_cmd = manifest.get('Service', 'accept_cmd')
+# sanity_cmd = manifest.get('Service', 'sanity_cmd')
 
 service_port = manifest.get('Service', 'service_port')
 docs_port = manifest.get('Service', 'docs_port')
@@ -24,31 +22,31 @@ ACCEPT_HOST = accept_host_addr.split(':')[0]
 ACCEPT_PORT = accept_host_addr.split(':')[1]
 
 def up():
+    """ Bring up the local dev environment """
     local('vagrant up')
 
 def down():
+    """ Destroy the local dev environment """
     local('vagrant destroy')
 
 def test(build_name=None):
+    """ Run the unit tests in a local build """
     image_name = make_image_name(build_name)
     build(image_name)
     vagrant("docker run {image_name} {cmd}".format(
                 image_name=image_name, cmd=unittest_cmd))
 
-def accept(build_name=None):
-    image_name = make_image_name(build_name)
-    build(image_name)
-    vagrant("docker run -d -p 127.0.0.1:{}:{} {}".format(
-        service_port, service_port, image_name))
-    try:
-        vagrant(accept_cmd)
-    except Exception, e:
-        raise e
-    finally:
-        vagrant("docker stop `docker ps -q`")
-        vagrant("docker rm `docker ps -aq`")
-
 def integrate(build_name=None):
+    """
+    Run the continuous integration workflow
+
+    1. Pull in any new mainline changes
+    2. Pull in any new current branch changese
+    3. Build locally and test
+    4. Push local code changes to remote hub repo (current branch)
+    5. Push image to docker index
+
+    """
 
     # Merge any new mainline changes
     local("git pull hub mainline")
@@ -66,16 +64,13 @@ def integrate(build_name=None):
     # push passed code changes to current branch
     local("git push -u hub {}".format(branch))
 
-    # trigger the build server for this image
+    # push passed image to the docker index
     image_name = make_image_name(build_name)
     vagrant("docker push {image_name}".format(image_name=image_name))
-    accept_trigger = "curl localhost:{}/{}?{}".format(ACCEPT_PORT,
-                                                      urllib.quote(image_name),
-                                                      urllib.quote(accept_cmd),
-                                                      )
-    with settings(host_string=ACCEPT_HOST):
-        run(accept_trigger)
 
+    #TODO trigger acceptance testing in the Build server
+
+""" probably will move all deploy tasks out of fabric
 def deploy_local(image_name, port):
     build(image_name)
     run_image_on_port(vagrant, image_name, port)
@@ -84,16 +79,24 @@ def deploy(image_name, port):
     with settings(host_string=app_host):
         run("docker pull {image_name}".format(image_name=image_name))
         run_image_on_port(run, image_name, port)
+"""
 
 def build(image_name):
+    """ build the Dockerfile with the given name """
     vagrant("docker build -t {image_name} .".format(image_name=image_name))
 
 def run_image_on_port(runner, image_name, port):
+    """ run the image and bind the exposed port to the given port """
     test(image_name)
     runner("docker run -p {port}:{docker_port} -i -t -d {image_name}".format(
             port=port, docker_port=exposed_port, image_name=image_name))
 
-def make_image_name(build_name):
+def make_image_name(build_name=''):
+    """
+    make an image name based on the given build name and current git state
+
+    """
+
     # ensure that the name of the resulting image matches the git
     # checkout in either the commit hash or a tag
     if not build_name:
@@ -113,4 +116,5 @@ def make_image_name(build_name):
     return image_name
 
 def vagrant(cmd):
+    """ send a command to the vagrant box """
     local("vagrant ssh -c 'cd /vagrant && {}'".format(cmd))
