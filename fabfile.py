9da43ab304ceb340814c197f8fd106b8a4b82104
Jesse B Miller <jmiller@adops.com>
2014-10-08 16:28:17 -0500

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -14,7 +14,7 @@ service_port = manifest.get('Service', 'service_port')
 # coverage_port = manifest.get('Service', 'coverage_port')
 
 registry_host_addr = '104.130.3.209:5000'
-accept_host_addr = '104.130.3.209:5001'
+# accept_host_addr = '104.130.3.209:5001'
 
 REGISTRY_HOST = registry_host_addr.split(':')[0]
 REGISTRY_PORT = registry_host_addr.split(':')[1]
